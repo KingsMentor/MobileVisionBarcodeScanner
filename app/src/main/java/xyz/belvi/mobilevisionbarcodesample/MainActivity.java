f567772571937a306578b9abbd8a23fba632fee5
Nosakhare Belvi <nosakharebelvi@gmail.com>
2016-10-12 00:03:36 +0100

    supporting multiple barcode format and adding failure callback for barcodeRetriever

--- app/src/main/java/xyz/belvi/mobilevisionbarcodesample/MainActivity.java
+++ app/src/main/java/xyz/belvi/mobilevisionbarcodesample/MainActivity.java
@@ -16,15 +16,13 @@
 
 package xyz.belvi.mobilevisionbarcodesample;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.util.SparseArray;
 import android.view.View;
-import android.widget.TextView;
 
-import com.google.android.gms.common.api.CommonStatusCodes;
 import com.google.android.gms.samples.vision.barcodereader.BarcodeCapture;
 import com.google.android.gms.samples.vision.barcodereader.BarcodeGraphic;
 import com.google.android.gms.vision.barcode.Barcode;
@@ -40,8 +38,7 @@ import xyz.belvi.mobilevisionbarcodescanner.BarcodeRetriever;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener, BarcodeRetriever {
 
     // use a compound button so either checkbox or switch widgets work.
-    private TextView statusMessage;
-    private TextView barcodeValue;
+
 
     private static final int RC_BARCODE_CAPTURE = 9001;
     private static final String TAG = "BarcodeMain";
@@ -51,11 +48,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        statusMessage = (TextView) findViewById(R.id.status_message);
-        barcodeValue = (TextView) findViewById(R.id.barcode_value);
 
-
-        findViewById(R.id.read_barcode).setOnClickListener(this);
 
         BarcodeCapture barcodeCapture = (BarcodeCapture) getSupportFragmentManager().findFragmentById(R.id.barcode);
         barcodeCapture.setRetrieval(this);
@@ -69,64 +62,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
      */
     @Override
     public void onClick(View v) {
-        if (v.getId() == R.id.read_barcode) {
-            // launch barcode activity.
-//            Intent intent = new Intent(this, BarcodeCaptureActivity.class);
-//            intent.putExtra(BarcodeCaptureActivity.AutoFocus, autoFocus.isChecked());
-//            intent.putExtra(BarcodeCaptureActivity.UseFlash, useFlash.isChecked());
-//
-//            startActivityForResult(intent, RC_BARCODE_CAPTURE);
-        }
 
     }
 
-    /**
-     * Called when an activity you launched exits, giving you the requestCode
-     * you started it with, the resultCode it returned, and any additional
-     * data from it.  The <var>resultCode</var> will be
-     * {@link #RESULT_CANCELED} if the activity explicitly returned that,
-     * didn't return any result, or crashed during its operation.
-     * <p/>
-     * <p>You will receive this call immediately before onResume() when your
-     * activity is re-starting.
-     * <p/>
-     *
-     * @param requestCode The integer request code originally supplied to
-     *                    startActivityForResult(), allowing you to identify who this
-     *                    result came from.
-     * @param resultCode  The integer result code returned by the child activity
-     *                    through its setResult().
-     * @param data        An Intent, which can return result data to the caller
-     *                    (various data can be attached to Intent "extras").
-     * @see #startActivityForResult
-     * @see #createPendingResult
-     * @see #setResult(int)
-     */
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == RC_BARCODE_CAPTURE) {
-            if (resultCode == CommonStatusCodes.SUCCESS) {
-                if (data != null) {
-                    Barcode barcode = data.getParcelableExtra(BarcodeCapture.BarcodeObject);
-                    statusMessage.setText(R.string.barcode_success);
-                    barcodeValue.setText(barcode.displayValue);
-                    Log.d(TAG, "Barcode read: " + barcode.displayValue);
-                } else {
-                    statusMessage.setText(R.string.barcode_failure);
-                    Log.d(TAG, "No barcode captured, intent data is null");
-                }
-            } else {
-                statusMessage.setText(String.format(getString(R.string.barcode_error),
-                        CommonStatusCodes.getStatusCodeString(resultCode)));
-            }
-        } else {
-            super.onActivityResult(requestCode, resultCode, data);
-        }
-    }
 
     @Override
     public void onRetrieved(Barcode barcode) {
         Log.d(TAG, "Barcode read: " + barcode.displayValue);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this)
+                .setTitle("code retrieved")
+                .setMessage(barcode.displayValue);
+        builder.show();
     }
 
     @Override
